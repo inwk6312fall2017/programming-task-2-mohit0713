88d9a9f8abca33b2c4f00bb11051a53946d6500a
inwkstudent <nisarg.gajjar@dal.ca>
2017-10-02 12:31:44 -0300

    iterating within the file to get unique values

--- task1.py
+++ task1.py
@@ -1,8 +1,10 @@
 import csv
-x = open("Crime.csv", 'r') # opens the file read-only mode
+x = open("Crime.csv", 'r') # opens the file read-only
 
-
+crime_type, no_of_times = [], [] # creating 2 list
 def extract_info(w): # function to get the desired info from file
+	if  row[8] in w:
+		crime_type.append(row[8])
 	c = 0
 	a = "ASSAULT"
 	for a in w:
