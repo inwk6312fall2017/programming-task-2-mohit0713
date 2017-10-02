2865def620731695019f8c1cc7f1e3b9df5d7cb4
inwkstudent <nisarg.gajjar@dal.ca>
2017-10-02 12:10:11 -0300

    starting of the program

---
+++ task1.py
@@ -0,0 +1,12 @@
+x = open(crime.csv, "r") # opens the file read-only mode
+
+
+def extract_info(): # function to get the desired info from file
+	#something to iterate withing th file
+	return # the informtion needed
+
+extract_info() # call the function
+
+
+
+
