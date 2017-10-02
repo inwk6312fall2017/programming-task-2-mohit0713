cda80bc67d92de1bdf5cfaac3bcfe71efd4cc82c
inwkstudent <nisarg.gajjar@dal.ca>
2017-10-02 12:55:51 -0300

    creating a new function for finding unique crimes

--- task1.py
+++ task1.py
@@ -3,18 +3,21 @@ x = open("Crime.csv", 'r') # opens the file read-only
 
 crime_type, no_of_times = [], [] # creating 2 list
 
+a = "ASSAULT"
 
+def new_func (): #creates a list of unique crimes
+	# something
 
 def extract_info(w): # function to get the desired info from file
 #	if  row[8] in w:
-#		crime_type.append(row[8])
-#	c = 0
+#		crime_type.append(row[8])y
+	c = 0
 #	for i in len(crime_type): #loop that goes for the len of the list created
-	for i in w: # loop that check the value in the list in the file
+	for a in w: # loop that check the value in the list in the file
 		c+=1 # couter that increament the value
-		no_of_times.append(c) # appends the list with number of times the 
+		 # appends the list with number of times the 
 #	print (crime_type)
-	print (no_of_times)
+	print (c)
 
 extract_info(x) # call the function
 
